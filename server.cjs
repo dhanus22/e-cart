245e1f9a202a06e4da057f96b6bfe8db685aa9c3
dhanus22 <smsaidhanush153@gmail.com>
2026-04-13 10:33:51 +0530

    server path change

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('src/Db.json')
+const router = jsonServer.router('Db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
